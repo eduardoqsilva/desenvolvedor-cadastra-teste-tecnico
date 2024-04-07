e1e3618331d544a2dbf9ec6029f3657e61cdbcf4
Eduardo Q <eduadsilva@outlook.com>
2024-04-07 03:13:08 -0300

    add a default theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyled } from './styles/global.styled.ts'
+import { defaultTheme } from './styles/themes/default.ts'
 import './styles/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyled />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,14 @@
+export const defaultTheme = {
+  font: {
+    default: '"Open Sans", sans-serif',
+  },
+  
+  colors: {
+    primary: '#FB953E',
+    black: '#000000',
+    gray01: '#00000080',
+    gray02: '#666666',
+
+    white: '#FFFF',
+  },
+}
