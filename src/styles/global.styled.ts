15ff2fe566d541538de509228c9b3d6df475e197
Eduardo Q <eduadsilva@outlook.com>
2024-04-07 22:53:47 -0300

    update a global theme and styled

--- src/styles/global.styled.ts
+++ src/styles/global.styled.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyled = createGlobalStyle`
   *{
@@ -8,7 +8,45 @@ export const GlobalStyled = createGlobalStyle`
   }
 
   body {
-    font-family: ${props => props.theme.font.default};
-    color: ${props => props.theme.colors.black};
+    font-family: ${(props) => props.theme.font.default};
+    color: ${(props) => props.theme.colors.black};
+    background-color: ${(props) => props.theme.colors.white01};
+  }
+
+  /* Firefox (uncomment to work in Firefox, although other properties will not work!)  */
+  /** {
+  scrollbar-width: thin;
+  }*/
+
+  /* Chrome, Edge and Safari */
+  *::-webkit-scrollbar {
+    height: 10px;
+    width: 10px;
+    margin-top: 10px;
+  }
+  *::-webkit-scrollbar-track {
+    border-radius: 5px;
+    background-color: ${(props) => props.theme.colors.white01};
+  }
+
+  *::-webkit-scrollbar-track:hover {
+    background-color: ${(props) => props.theme.colors.white01};
+  }
+
+  *::-webkit-scrollbar-track:active {
+    background-color: ${(props) => props.theme.colors.white01};
+  }
+
+  *::-webkit-scrollbar-thumb {
+    border-radius: 5px;
+    background-color:${(props) => props.theme.colors.primary};
+  }
+
+  *::-webkit-scrollbar-thumb:hover {
+    background-color: ${(props) => props.theme.colors.primary};
+  }
+
+  *::-webkit-scrollbar-thumb:active {
+    background-color:${(props) => props.theme.colors.primary};
   }
 `
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -2,13 +2,14 @@ export const defaultTheme = {
   font: {
     default: '"Open Sans", sans-serif',
   },
-  
+
   colors: {
     primary: '#FB953E',
     black: '#000000',
     gray01: '#00000080',
     gray02: '#666666',
 
-    white: '#FFFF',
+    white01: '#FFFF',
+    white02: '#f6f6f6',
   },
 }
