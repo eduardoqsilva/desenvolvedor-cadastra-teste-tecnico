06ac994e20efd8959be29296520ef6de87566e01
Eduardo Q <eduadsilva@outlook.com>
2024-04-11 02:16:08 -0300

    update a types from Image component

--- src/components/image/imageTypes.d.ts
+++ src/components/image/imageTypes.d.ts
@@ -3,6 +3,5 @@ import { ImgHTMLAttributes } from 'react'
 export interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
   objectFit?: 'cover' | 'contain' | 'fill'
   loadIconSize?: `${number}%`
-  refUrl?: string
   className?: string
 }
