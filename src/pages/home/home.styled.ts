49e54adbdd476f1932ca7626450a0d228a25da87
Eduardo Q <eduadsilva@outlook.com>
2024-04-11 02:17:00 -0300

    update a styleds from home page

--- src/pages/home/home.styled.ts
+++ src/pages/home/home.styled.ts
@@ -5,11 +5,12 @@ export const HomeContainer = styled.div`
   min-height: 100vh;
   max-height: 100vh;
   overflow: hidden;
+  display: flex;
+  flex-direction: column;
 `
 export const HomeContent = styled.div`
   width: 100%;
-  height: calc(100vh - 91px);
-  display: flex;
+  flex: 1;
 
   overflow-y: scroll;
 `
@@ -19,26 +20,42 @@ export const HomeGridWrapper = styled.div`
   max-width: 1200px;
   height: 100%;
   margin: 0 auto;
-  flex: 1;
 
   display: grid;
-  grid-template-columns: 1fr 4fr;
+  grid-template-columns: 2fr 6fr 1fr;
   grid-template-rows: 100%;
   grid-template-areas: 'filters products';
-  gap: 180px;
+  gap: 32px;
 `
 
 export const CardsGrid = styled.main`
   width: 100%;
-  height: 100%;
+  height: min-content;
 
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: auto;
+  grid-template-rows: fit-content;
   grid-area: products;
-  gap: 32px;
+  gap: 24px;
+  margin-bottom: 32px;
 
   @media (max-width: 800px) {
     grid-template-columns: repeat(2, 1fr);
   }
+`
+
+export const TitleCategory = styled.h1`
+  font-size: 2rem;
+  font-weight: 400;
+  line-height: 43.58px;
+
+  max-width: 1300px;
+  margin: 40px auto;
+`
+
+export const OrderByContainer = styled.div`
+  width: 100%;
+  height: fit-content;
+
+  position: relative;
 `
