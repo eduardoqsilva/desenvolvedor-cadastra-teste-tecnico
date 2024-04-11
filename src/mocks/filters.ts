51e7a9ca2568b67a22ace0e457fd155701714e8c
Eduardo Q <eduadsilva@outlook.com>
2024-04-11 02:18:06 -0300

    add a mock filters const

---
+++ src/mocks/filters.ts
@@ -0,0 +1,22 @@
+export const Colors = [
+  { text: 'Amarelo', value: 'Amarelo' },
+  { text: 'Azul', value: 'Azul' },
+  { text: 'Branco', value: 'Branco' },
+  { text: 'Cinza', value: 'Cinza' },
+  { text: 'Laranja', value: 'Laranja' },
+  { text: 'Verde', value: 'Verde' },
+  { text: 'Vermelho', value: 'Vermelho' },
+  { text: 'Preto', value: 'Preto' },
+  { text: 'Rosa', value: 'Rosa' },
+  { text: 'Vinho', value: 'Vinho' },
+]
+
+export const Sizes = ['P', 'M', 'G', 'GG', 'U', '36', '38', '40']
+
+export const PriceRange = [
+  { text: 'de R$0 até R$50', value: '0-50' },
+  { text: 'de R$51 até R$150', value: '51-150' },
+  { text: 'de R$151 até R$300', value: '151-300' },
+  { text: 'de R$301 até R$500', value: '301-500' },
+  { text: 'a partir de R$ 500', value: '500' },
+]
